c5763004d676bfd638fa02cb706a182745a2b33d
Srinivasa8982 <80963791+Srinivasa8982@users.noreply.github.com>
2021-04-04 13:50:25 +0530

    added class srnivas

---
+++ sri/src/srnivasa.java
@@ -0,0 +1,14 @@
+
+public class srnivasa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hi sri");
+		System.out.println("Hi Raj");
+		System.out.println("Hi Raj");
+
+	}
+
+}
